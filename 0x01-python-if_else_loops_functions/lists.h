1d0472dc555745c9f024133c66671daf47574c6b
Youssehf <130256186+Youssehf@users.noreply.github.com>
2023-10-05 00:08:41 +0300

    Update lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,12 +2,8 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
