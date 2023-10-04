35bf2fea870ddc5be06322ae3ab503ad03422cd6
Youssehf <130256186+Youssehf@users.noreply.github.com>
2023-10-04 23:39:16 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    upper = []
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            upper.append(chr(ord(c) - 32))
+        else:
+            upper.append(c)
+    new_str = "".join(upper)
